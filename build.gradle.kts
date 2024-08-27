b6f460ec7ffb19dbb9b50deeb8eabbc44931daab
Vlad Denisov <vlad.a.denisov@gmail.com>
2024-08-28 00:47:13 +0300

    Add Initial Swagger configuration & db in docker-compose to run locally

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
 	implementation("org.liquibase:liquibase-core")
 	implementation ("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
 
 	runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.12.6")
 	runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.12.6")
